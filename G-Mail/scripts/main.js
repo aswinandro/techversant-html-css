915388d616b73ed92f0a5135781518f3a0c780cc
aswinandro <60426268+aswinandro@users.noreply.github.com>
2021-06-21 13:15:03 +0530

    G-Mail Replica Commit

---
+++ G-Mail/scripts/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+    // jQuery methods go here...
+    $("#open-modal").click(function(){
+        $("#modal").toggle();
+      });
+      
+      $("#close-modal").click(function(){
+        $("#modal").hide();
+      });
+      $("#close").click(function(){
+        $("#modal").hide();
+      });
+      $("#close").click(function(){
+        $("#modal").hide();
+      });
+    
+  });
